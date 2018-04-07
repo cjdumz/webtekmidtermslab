3c1e56ef0dabbc392bda9d0f1523ab8480323f91
Joshua <2140748@slu.edu.ph>
2018-04-08 00:15:03 +0800

    Added Service Worker Javascript File

---
+++ webteklabmid/theme/sw.js
@@ -0,0 +1,28 @@
+importScripts('/cache-polyfill.js');
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('theme').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/4teams.html',
+       '/8teams.html',
+       '/12teams.html',
+       '/assets/css/bootstrap.css',
+       '/assets/css/main.css',
+       '/assets/fonts/glyphicons-halflings-regular.eot',
+       '/assets/fonts/glyphicons-halflings-regular.ttf',
+       '/assets/js/bootstrap.min.js',
+       '/assets/js/hover.zoom.conf.js',
+       '/assets/js/hover.zoom.js'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(event) {
+
+console.log(event.request.url);
+
+});
